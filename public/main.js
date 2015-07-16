39e7f72f3b03c3f5d892e70dec87333549256f4d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-16 18:08:25 +0800

    use singal quotation replace double

--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 function dataPrinter(data) {
-    $("#tbody").empty();
+    $('#tbody').empty();
     data.forEach(function(val) {
-          $("<tr><td>" + val.id + "<td>" + val.name + "<td id= " + val.id + ">" + "delete" + "</td></tr>").appendTo("#tbody");
+          $('<tr><td>' + val.id + '<td>' + val.name + '<td id= ' + val.id + '>' + 'delete' + '</td></tr>').appendTo('#tbody');
       })
 }
 
 function getInfo() {
-    $.get("/getInfo", function(data) {
+    $.get('/getInfo', function(data) {
         dataPrinter(data);
     });
 }
@@ -14,23 +14,23 @@ function getInfo() {
 $(function() {
     getInfo();
 
-    $("#tbody").on("click", function(evt) {
+    $('#tbody').on('click', function(evt) {
         var id = evt.target.id;
 
-        if (id==='') {
+        if (id ==='') {
             return;
         }
         $.ajax({
-            url:"/deleteInfo",
-            type:"delete",
+            url:'/deleteInfo',
+            type:'delete',
             data:{id:id}
         });
         getInfo();
     });
 
-    $("#add").on("click",function(evt) {
-        $.post('/postInfo',{name:$("#addName").prop('value')},function() {});
+    $('#add').on('click',function(evt) {
+        $.post('/postInfo',{name:$('#addName').prop('value')},function() {});
         getInfo();
-        $("#addName").prop('value',"");
+        $('#addName').prop('value','');
     });
 });
